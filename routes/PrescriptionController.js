d60da22be9671c9c02a5c4decfb49e9b0b99f4f6
yornellas <yo_ornellas@hotmail.com>
2026-02-07 08:58:02 -0300

    feat: add bycript on password

--- routes/PrescriptionController.js
+++ routes/PrescriptionController.js
@@ -1,65 +1,79 @@
-import express from 'express';
-import PrescriptionService from '../services/PrescriptionService.js';
+import express from 'express'
+import PrescriptionService from '../services/PrescriptionService.js'
+import bycript from 'bcryptjs'
 
-let router = express.Router();
+let router = express.Router()
 
-router.get('/prescriptions', async(req, res) => {
+router.get('/prescriptions', async (req, res) => {
   try {
-    const prescriptions = await PrescriptionService.getPrescriptions();
-    res.send(prescriptions);
+    const prescriptions = await PrescriptionService.getPrescriptions()
+    res.send(prescriptions)
   } catch (error) {
     console.log(error)
     res.status(500).send(error)
   }
 })
 
-router.get('/getPrescription/:id', async(req, res) => {
-  const { id } = req.params;
+router.get('/getPrescription/:id', async (req, res) => {
+  const { id } = req.params
 
   try {
-    const prescription = await PrescriptionService.getPrescriptionById(id);
-    res.send(prescription);
+    const prescription = await PrescriptionService.getPrescriptionById(id)
+    res.send(prescription)
   } catch (error) {
     console.log(error)
     res.status(500).send(error)
   }
 })
 
-router.post('/postPrescription', async(req, res) => {
-  const { date, appointmentId, medicine, dosage, instructions } = req.body;
+router.post('/postPrescription', async (req, res) => {
+  const { date, appointmentId, medicine, dosage, instructions } = req.body
 
   try {
-    const prescription = await PrescriptionService.savePrescription({ date, appointmentId, medicine, dosage, instructions });
-    res.send(prescription);
+    const hashedPassword = await bycript.hash(password, 10)
+    const prescription = await PrescriptionService.savePrescription({
+      date,
+      appointmentId,
+      medicine,
+      dosage,
+      instructions,
+    })
+    res.send(prescription)
   } catch (error) {
     console.log(error)
     res.status(500).send(error)
   }
 })
 
-router.put('/prescriptions/:id', async(req, res) => {
-  const { id } = req.params;
-  const { date, appointmentId, medicine, dosage, instructions } = req.body;
+router.put('/prescriptions/:id', async (req, res) => {
+  const { id } = req.params
+  const { date, appointmentId, medicine, dosage, instructions } = req.body
 
   try {
-    const prescription = await PrescriptionService.updatePrescription(id, { date, appointmentId, medicine, dosage, instructions });
-    res.send(prescription);
+    const prescription = await PrescriptionService.updatePrescription(id, {
+      date,
+      appointmentId,
+      medicine,
+      dosage,
+      instructions,
+    })
+    res.send(prescription)
   } catch (error) {
     console.log(error)
     res.status(500).send(error)
   }
 })
 
-router.delete('/prescriptions/:id', async(req, res) => {
-  const { id } = req.params;
+router.delete('/prescriptions/:id', async (req, res) => {
+  const { id } = req.params
 
   try {
-    const prescription = await PrescriptionService.deletePrescription(id);
-    res.send(prescription);
+    const prescription = await PrescriptionService.deletePrescription(id)
+    res.send(prescription)
   } catch (error) {
     console.log(error)
     res.status(500).send(error)
   }
 })
 
-export default router;
+export default router
