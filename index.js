0e7c83c82545a7b723e9a88128d3ddbf6a6a39a2
yornellas <yo_ornellas@hotmail.com>
2026-02-07 10:22:28 -0300

    chore: run prettier

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import router from './routes/router.js';
-import db from './database/database.js';
+import express from 'express'
+import router from './routes/router.js'
+import db from './database/database.js'
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('Listen to port 3000')
-});
+})
 
-app.use('/', router);
+app.use('/', router)
