0c42ff7f69199c629bfb9694333356eef189d8f1
yornellas <yo_ornellas@hotmail.com>
2026-02-07 10:22:21 -0300

    feat: add reschedule appointment route

--- routes/AppointmentController.js
+++ routes/AppointmentController.js
@@ -70,4 +70,20 @@ router.delete('/appointments/:id', async (req, res) => {
   }
 })
 
+router.put('/reschedule/:id', async (req, res) => {
+  const { id } = req.params
+  const { date } = req.body
+
+  try {
+    let appointment = await AppointmentService.getAppointmentById(id)
+    appointment.date = date
+
+    appointment = await AppointmentService.updateAppointment(id, { date })
+    res.send(appointment)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error)
+  }
+})
+
 export default router
